4700f50f38908caadfbd788da4da4e794c746aa9
Chukwubuikem4 <emmanuelikwuka1@gmail.com>
2024-10-05 14:36:50 +0100

    Removing error classes

--- script.js
+++ script.js
@@ -57,7 +57,9 @@ const setErrorFor = (input, message) =>{
 const isMail = (email) =>{
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 
- }
+ };
+
+//  Removing errors
 
 
 
